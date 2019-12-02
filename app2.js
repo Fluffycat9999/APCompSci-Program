dcbc88a8ab998a9e37ab2a38f79fbcd2eb08deb9
Fluffycat9999 <50632021+Fluffycat9999@users.noreply.github.com>
2019-12-02 11:43:57 -0500

    Day 6 12-2-19(M)
    Still won't display.......

--- app2.js
+++ app2.js
@@ -1,10 +1,10 @@
 class bmi{
-    constructor(weight, height){
+    constructor(weight, height/* , result */){
         this.weight = weight;
-        this.height = height;
-        this.getBMI = function(){
+        this.height = height;/* 
+        this.result = result; *//* (function getBMI(){
         return this.bmi.weight / (this.bmi.height * this.bmi.height);
-    }
+    } )*/
 }
 }
 /* const Mine = new bmi(110, 60);
@@ -15,21 +15,24 @@ class UI{
     addBMIToList(bmi){
         const display = document.querySelector('.display');
         let html = '<div class="display-bmi"> <div class="display-weight"> %weight% </div> <div class="display-height"> %height% </div><div class="display-result">%result%</div></div><div class="remove-bmi"> <p class="remove-bmi">Remove BMI &#10006;</p> </div> </div>'
-        const getBMI = weight.value / (height.value * height.value);
+        
+        const getBMI = function(){
+            weight.value / (height.value * height.value)
+        };
         let newHtml= html.replace('%weight%', bmi.weight);
         newHtml = newHtml.replace('%height%', bmi.height);
-        newHtml = newHtml.replace('%result%', getBMI);
-        display.insertAdjacentHTML('beforeend', newHtml);  
-        console.log(getBMI);
+        newHtml = newHtml.replace('%result%', result);
+        display.insertAdjacentHTML('beforeend', newHtml);
+
     }
 }
 
 document.getElementById('form').addEventListener('submit', function(e){
     const weight = document.getElementById('weight').value;
-    const height = document.getElementById('height').value;
-    const result = document.getElementById('result').value;
+    const height = document.getElementById('height').value;/* 
+    const result = document.getElementById('result').value; */
 
-    const BMI = new bmi(weight, height, result);
+    const BMI = new bmi(weight, height/* , result */);
     console.log(bmi);
 
     const ui = new UI();
